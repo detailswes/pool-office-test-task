709a714d0f9ab1e951a0d44abb6285e6d47fc460
details.wes <details.wes@gmail.com>
2023-12-08 13:24:28 +0530

    Changed loader

--- src/common/Loader.js
+++ src/common/Loader.js
@@ -1,18 +1,18 @@
+import ClipLoader from "react-spinners/ClipLoader";
+import styles from "../common/common.module.css";
 
-import LoadingOverlay from 'react-loading-overlay'
-import { SpinnerCircular } from 'spinners-react'
-import styles from "../common/common.module.css"
 const Loader = ({ active }) => {
+  return (
+    <div className={styles.loader}>
+      <ClipLoader
+        color="#00b3ff"
+        loading={active}
+        size={25}
+        speedMultiplier={0.6}
+      />
+      {active && <p>Fetching favorite star...</p>}
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.singleField}>
-            <LoadingOverlay
-                active={active}
-                spinner={<SpinnerCircular size={32} thickness={80} speed={150} color="rgba(57, 62, 172, 1)" secondaryColor="rgba(57, 172, 125, 0.44)" />}
-                text='Fetching favorite star...'
-            />
-        </div>
-    )
-}
-
-export default Loader
+export default Loader;
